b52c2a1fa9102fca9596eb3b1c739d93dabdfd90
David <davidchiva3@gmail.com>
2016-10-03 19:21:12 +0200

    modificado método isImproper

--- src/main/java/es/upm/miw/spai/ecp2/Fraction.java
+++ src/main/java/es/upm/miw/spai/ecp2/Fraction.java
@@ -63,7 +63,7 @@ public class Fraction {
     }
 
 	public Object isImproper() {
-		return numerator>=denominator;
+		return this.getNumerator()>this.getDenominator();
 	}
 
 	public Object isProper() {
